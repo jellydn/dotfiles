8018aa7e006f49555bbe7769c34291a78d6787b3
Kaka Huynh <dunghd.it@gmail.com>
2019-05-31 21:00:27 +0900

    feat(nvim): update install nvim

--- nvim.sh
+++ nvim.sh
@@ -1,4 +1,6 @@
 #!/bin/sh
+brew install python3
+brew install neovim
 cp init.vim ~/.config/nvim/init.vim
 cp .vimrc.local ~/.config/nvim/local_init.vim
 cp .vimrc.local.bundles ~/.config/nvim/local_bundles.vim

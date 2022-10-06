271a37b923e2b589e10ad4cc6d47c6c66bb9cdbf
Huynh Duc Dung <dunghd.it@gmail.com>
2022-10-06 22:23:53 +0800

    :sparkles: add mapping for hop plugin
    Refer to advance Hop motions https://github.com/phaazon/hop.nvim/wiki/Advanced-Hop

--- nvchad-custom/mappings.lua
+++ nvchad-custom/mappings.lua
@@ -24,9 +24,42 @@ M.hop = {
   n = {
     ["f"] = {
       function()
-        require("hop").hint_char1()
+        require("hop").hint_char1 {
+          direction = require("hop.hint").HintDirection.AFTER_CURSOR,
+        }
       end,
-      "hope mode",
+      "hope forward",
+      opts = { nowait = true },
+    },
+    ["F"] = {
+      function()
+        require("hop").hint_char1 {
+          direction = require("hop.hint").HintDirection.BEFORE_CURSOR,
+        }
+      end,
+      "hope backwards",
+      opts = { nowait = true },
+    },
+    ["t"] = {
+      function()
+        require("hop").hint_char1 {
+          direction = require("hop.hint").HintDirection.AFTER_CURSOR,
+          current_line_only = true,
+          hint_offset = -1,
+        }
+      end,
+      "hope target forward",
+      opts = { nowait = true },
+    },
+    ["T"] = {
+      function()
+        require("hop").hint_char1 {
+          direction = require("hop.hint").HintDirection.BEFORE_CURSOR,
+          current_line_only = true,
+          hint_offset = -1,
+        }
+      end,
+      "hope target backwards",
       opts = { nowait = true },
     },
   },

b73274c66fef9674609d1617e3768fe98f7c411b
Huynh Duc Dung <dunghd.it@gmail.com>
2022-12-08 10:51:42 +0800

    feat(nvim): add eslint_d condition
    This condition checks if there is an eslint config file in the current
    directory. If there is, then the eslint_d source is enabled.

--- nvchad-custom/plugins/null-ls.lua
+++ nvchad-custom/plugins/null-ls.lua
@@ -6,6 +6,24 @@ end
 
 local b = null_ls.builtins
 
+local function eslint_config_exists()
+  local eslintrc = vim.fn.glob(".eslintrc*", 0, 1)
+
+  if not vim.tbl_isempty(eslintrc) then
+    return true
+  end
+
+  local current_dir = vim.fn.getcwd()
+  local pkg_json = current_dir .. "/package.json"
+  if vim.fn.filereadable(pkg_json) == 1 then
+    if vim.fn.json_decode(vim.fn.readfile(pkg_json))["eslintConfig"] then
+      return true
+    end
+  end
+
+  return false
+end
+
 local sources = {
 
   -- spell check
@@ -17,15 +35,19 @@ local sources = {
   b.code_actions.proselint,
 
   -- webdev stuff
-  b.diagnostics.eslint_d,
+  b.diagnostics.eslint_d.with {
+    condition = function()
+      return eslint_config_exists()
+    end,
+  },
   b.code_actions.eslint_d,
   b.formatting.eslint_d,
   -- b.formatting.deno_fmt.with({
-  --   	filetypes = { "javascript", "javascriptreact", "json", "jsonc", "typescript", "typescriptreact" }, 
+  --   	filetypes = { "javascript", "javascriptreact", "json", "jsonc", "typescript", "typescriptreact" },
   -- }),
   -- TODO: install romejs if possible
   b.formatting.prettierd.with {
-    filetypes = { "html", "markdown", "css" },
+    filetypes = { "javascript", "javascriptreact", "json", "jsonc", "typescript", "typescriptreact" },
   },
 
   -- Lua

81d210a0b1e6bf9c1ffe2c970d757615ba681725
Huynh Duc Dung <dunghd.it@gmail.com>
2023-01-14 23:14:08 +0800

    fix(nvim): remove todo plugins

--- nvchad-custom/plugins/init.lua
+++ nvchad-custom/plugins/init.lua
@@ -1,179 +1,171 @@
-local overrides = require "custom.plugins.overrides"
+local overrides = require("custom.plugins.overrides")
 
 return {
 
-  -- format on save with LSP
-  ["lukas-reineke/lsp-format.nvim"] = {
-    config = function()
-      require("lsp-format").setup {}
-    end,
-  },
+	-- format on save with LSP
+	["lukas-reineke/lsp-format.nvim"] = {
+		config = function()
+			require("lsp-format").setup({})
+		end,
+	},
 
-  -- NOTE: install spellcheck if needed
-  --
-  -- markdown
-  --
-  -- lazygit
-  ["kdheepak/lazygit.nvim"] = {
-    requires = "nvim-lua/plenary.nvim",
-  },
+	-- NOTE: install spellcheck if needed
+	--
+	-- markdown
+	--
+	-- lazygit
+	["kdheepak/lazygit.nvim"] = {
+		requires = "nvim-lua/plenary.nvim",
+	},
 
-  -- mason plugin
-  ["williamboman/mason-lspconfig.nvim"] = {
-    after = "mason.nvim",
-    config = function()
-      require("mason-lspconfig").setup()
-    end,
-  },
+	-- mason plugin
+	["williamboman/mason-lspconfig.nvim"] = {
+		after = "mason.nvim",
+		config = function()
+			require("mason-lspconfig").setup()
+		end,
+	},
 
-  -- search and replace
-  -- TODO: add plugin if needed
+	-- search and replace
+	-- TODO: add plugin if needed
 
-  -- highlight todo on comment
-  ["folke/todo-comments.nvim"] = {
-    requires = "nvim-lua/plenary.nvim",
-    config = function()
-      require("todo-comments").setup {}
-    end,
-  },
+	-- lsp plugin
+	["glepnir/lspsaga.nvim"] = {
+		branch = "main",
+		config = function()
+			local saga = require("lspsaga")
 
-  -- lsp plugin
-  ["glepnir/lspsaga.nvim"] = {
-    branch = "main",
-    config = function()
-      local saga = require "lspsaga"
+			saga.init_lsp_saga({
+				-- your configuration
+			})
+		end,
+	},
 
-      saga.init_lsp_saga {
-        -- your configuration
-      }
-    end,
-  },
+	-- copilot
+	["github/copilot.vim"] = {},
 
-  -- copilot
-  ["github/copilot.vim"] = {},
+	-- tabnine auto completion
+	["hrsh7th/nvim-cmp"] = {
+		config = function()
+			require("plugins.configs.cmp")
+			require("custom.plugins.configs.tabnine")
+			require("custom.plugins.configs.cmp").cmp()
+		end,
+	},
 
-  -- tabnine auto completion
-  ["hrsh7th/nvim-cmp"] = {
-    config = function()
-      require "plugins.configs.cmp"
-      require "custom.plugins.configs.tabnine"
-      require("custom.plugins.configs.cmp").cmp()
-    end,
-  },
+	["tzachar/cmp-tabnine"] = {
+		after = "nvim-cmp",
+		run = "./install.sh",
+		requires = "hrsh7th/nvim-cmp",
+		config = function()
+			require("custom.plugins.configs.tabnine")
+		end,
+	},
 
-  ["tzachar/cmp-tabnine"] = {
-    after = "nvim-cmp",
-    run = "./install.sh",
-    requires = "hrsh7th/nvim-cmp",
-    config = function()
-      require "custom.plugins.configs.tabnine"
-    end,
-  },
+	-- outline
+	["stevearc/aerial.nvim"] = {
+		after = "telescope.nvim",
+		config = function()
+			require("telescope").load_extension("aerial")
+			require("aerial").setup({
+				-- optionally use on_attach to set keymaps when aerial has attached to a buffer
+				on_attach = function(bufnr)
+					-- Jump forwards/backwards with '{' and '}'
+					vim.keymap.set("n", "{", "<cmd>AerialPrev<CR>", { buffer = bufnr })
+					vim.keymap.set("n", "}", "<cmd>AerialNext<CR>", { buffer = bufnr })
+				end,
+			})
+		end,
+	},
 
-  -- outline
-  ["stevearc/aerial.nvim"] = {
-    after = "telescope.nvim",
-    config = function()
-      require("telescope").load_extension "aerial"
-      require("aerial").setup {
-        -- optionally use on_attach to set keymaps when aerial has attached to a buffer
-        on_attach = function(bufnr)
-          -- Jump forwards/backwards with '{' and '}'
-          vim.keymap.set("n", "{", "<cmd>AerialPrev<CR>", { buffer = bufnr })
-          vim.keymap.set("n", "}", "<cmd>AerialNext<CR>", { buffer = bufnr })
-        end,
-      }
-    end,
-  },
+	-- enables dashboard
+	["goolord/alpha-nvim"] = {
+		disable = false,
+		override_options = overrides.alpha,
+	},
 
-  -- enables dashboard
-  ["goolord/alpha-nvim"] = {
-    disable = false,
-    override_options = overrides.alpha,
-  },
+	-- Override plugin definition options
+	["neovim/nvim-lspconfig"] = {
+		config = function()
+			require("plugins.configs.lspconfig")
+			require("custom.plugins.lspconfig")
+		end,
+	},
 
-  -- Override plugin definition options
-  ["neovim/nvim-lspconfig"] = {
-    config = function()
-      require "plugins.configs.lspconfig"
-      require "custom.plugins.lspconfig"
-    end,
-  },
+	-- rusttool
+	["simrat39/rust-tools.nvim"] = {
+		override_options = overrides.rusttool,
+	},
 
-  -- rusttool
-  ["simrat39/rust-tools.nvim"] = {
-    override_options = overrides.rusttool,
-  },
+	-- golang
+	["crispgm/nvim-go"] = {
+		requires = "nvim-lua/plenary.nvim",
+		config = function()
+			require("go").config.update_tool("quicktype", function(tool)
+				tool.pkg_mgr = "yarn"
+			end)
+		end,
+	},
 
-  -- golang
-  ["crispgm/nvim-go"] = {
-    requires = "nvim-lua/plenary.nvim",
-    config = function()
-      require("go").config.update_tool("quicktype", function(tool)
-        tool.pkg_mgr = "yarn"
-      end)
-    end,
-  },
+	-- overrde plugin configs
+	["nvim-treesitter/nvim-treesitter"] = {
+		override_options = overrides.treesitter,
+	},
 
-  -- overrde plugin configs
-  ["nvim-treesitter/nvim-treesitter"] = {
-    override_options = overrides.treesitter,
-  },
+	["williamboman/mason.nvim"] = {
+		override_options = overrides.mason,
+	},
 
-  ["williamboman/mason.nvim"] = {
-    override_options = overrides.mason,
-  },
+	["kyazdani42/nvim-tree.lua"] = {
+		override_options = overrides.nvimtree,
+	},
 
-  ["kyazdani42/nvim-tree.lua"] = {
-    override_options = overrides.nvimtree,
-  },
+	["max397574/better-escape.nvim"] = {
+		event = "InsertEnter",
+		config = function()
+			require("better_escape").setup()
+		end,
+	},
 
-  ["max397574/better-escape.nvim"] = {
-    event = "InsertEnter",
-    config = function()
-      require("better_escape").setup()
-    end,
-  },
+	-- autoclose tags in html, jsx only
+	["windwp/nvim-ts-autotag"] = {
+		ft = { "html", "javascriptreact" },
+		after = "nvim-treesitter",
+		config = function()
+			local present, autotag = pcall(require, "nvim-ts-autotag")
 
-  -- autoclose tags in html, jsx only
-  ["windwp/nvim-ts-autotag"] = {
-    ft = { "html", "javascriptreact" },
-    after = "nvim-treesitter",
-    config = function()
-      local present, autotag = pcall(require, "nvim-ts-autotag")
+			if present then
+				autotag.setup()
+			end
+		end,
+	},
 
-      if present then
-        autotag.setup()
-      end
-    end,
-  },
+	-- code formatting, linting etc
+	["jose-elias-alvarez/null-ls.nvim"] = {
+		after = "nvim-lspconfig",
+		config = function()
+			require("custom.plugins.null-ls")
+		end,
+	},
 
-  -- code formatting, linting etc
-  ["jose-elias-alvarez/null-ls.nvim"] = {
-    after = "nvim-lspconfig",
-    config = function()
-      require "custom.plugins.null-ls"
-    end,
-  },
+	-- jump motions on speed
+	["phaazon/hop.nvim"] = {
+		branch = "v2",
+		config = function()
+			require("hop").setup({})
+		end,
+	},
 
-  -- jump motions on speed
-  ["phaazon/hop.nvim"] = {
-    branch = "v2",
-    config = function()
-      require("hop").setup {}
-    end,
-  },
+	-- Add/change/delete surrounding delimiter pairs with ease.
+	["kylechui/nvim-surround"] = {
+		tag = "*", -- Use for stability; omit to use `main` branch for the latest features
+		config = function()
+			require("nvim-surround").setup({})
+		end,
+	},
 
-  -- Add/change/delete surrounding delimiter pairs with ease.
-  ["kylechui/nvim-surround"] = {
-    tag = "*", -- Use for stability; omit to use `main` branch for the latest features
-    config = function()
-      require("nvim-surround").setup {}
-    end,
-  },
+	-- remove plugin
+	["NvChad/nvterm"] = false,
 
-  -- remove plugin
-  ["NvChad/nvterm"] = false,
-
-  ["folke/which-key.nvim"] = false,
+	["folke/which-key.nvim"] = false,
 }

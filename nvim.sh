7a5a9c2c9eecd43f5a21b5144280eb6eb30348df
Huynh Duc Dung <dunghd.it@gmail.com>
2023-02-18 19:55:40 +0800

    chore: remove unused files

--- nvim.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-brew install python3
-brew install neovim
-mkdir ~/.config/nvim
-cp init.vim ~/.config/nvim/init.vim
-cp .vimrc.local ~/.config/nvim/local_init.vim
-cp .vimrc.local.bundles ~/.config/nvim/local_bundles.vim
-pip3 install --user --upgrade neovim
-nvim +PlugInstall +qall
--- vim.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-cp .vimrc ~/.vimrc
-cp .vimrc.local ~/.vimrc.local
-cp .vimrc.local.bundles ~/.vimrc.local.bundles
-vim +PlugInstall +qall

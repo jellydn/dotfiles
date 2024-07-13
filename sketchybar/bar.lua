43fcf8ad0d795657fba7c66da7a2761763d8caec
Huynh Duc Dung <dunghd.it@gmail.com>
2024-07-13 21:42:55 +0800

    feat: keep sketchychar on top

--- sketchybar/bar.lua
+++ sketchybar/bar.lua
@@ -2,11 +2,12 @@ local colors = require("colors")
 
 -- Equivalent to the --bar domain
 sbar.bar({
-	-- topmost = "window",
+	topmost = "off",
+	position = "top",
 	height = 30,
 	color = colors.transparent,
 	shadow = true,
-	sticky = true,
+	sticky = false,
 	padding_right = 10,
 	padding_left = 10,
 	blur_radius = 10,

506ea7e2b8712d4198b7cf645aab757c49911433
Huynh Duc Dung <dunghd.it@gmail.com>
2024-07-15 20:54:49 +0800

    feat: add kanagawa theme for sketchybar

--- sketchybar/colors.lua
+++ sketchybar/colors.lua
@@ -1,25 +1,26 @@
+-- Base on https://github.com/rebelot/kanagawa.nvim
 return {
-	black = 0xff181819,
-	white = 0xffe2e2e3,
-	red = 0xfffc5d7c,
-	green = 0xff9ed072,
-	blue = 0xff76cce0,
-	yellow = 0xffe7c664,
-	orange = 0xfff39660,
-	magenta = 0xffb39df3,
-	grey = 0xff7f8490,
+	black = 0xff090618,
+	white = 0xffc8c093,
+	red = 0xffc34043,
+	green = 0xff76946a,
+	blue = 0xff7e9cd8,
+	yellow = 0xffc0a36e,
+	orange = 0xffFFA066,
+	magenta = 0xff957fb8,
+	grey = 0xff727169,
 	transparent = 0x00000000,
 
 	bar = {
-		bg = 0xf02c2e34,
-		border = 0xff2c2e34,
+		bg = 0xff223249,
+		border = 0xff54546D,
 	},
 	popup = {
-		bg = 0xc02c2e34,
-		border = 0xff7f8490,
+		bg = 0xff2d4f67,
+		border = 0xff54546D,
 	},
-	bg1 = 0xff363944,
-	bg2 = 0xff414550,
+	bg1 = 0xf016161D,
+	bg2 = 0xff1f1f28,
 
 	with_alpha = function(color, alpha)
 		if alpha > 1.0 or alpha < 0.0 then

fb2b11e19e2dee3486e1a642b8a1463fe6794f91
Dung Huynh Duc <dunghd.it@gmail.com>
2025-09-19 10:18:20 +0800

    feat(fish): add pnpm to PATH in config.fish

--- common/.config/fish/config.fish
+++ common/.config/fish/config.fish
@@ -103,3 +103,10 @@ set -g fish_pager_color_description $comment
 if test -f ~/.local/try.rb
     ~/.local/try.rb init ~/src/tries | string collect | source
 end
+
+# pnpm
+set -gx PNPM_HOME "/home/dunghuynhduc/.local/share/pnpm"
+if not string match -q -- $PNPM_HOME $PATH
+  set -gx PATH "$PNPM_HOME" $PATH
+end
+# pnpm end
